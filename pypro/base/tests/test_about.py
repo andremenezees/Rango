0b14a3a23af9803b2059c2bffb96f639b6858eab
andremenezees <andrr96@gmail.com>
2020-07-14 02:41:51 -0300

    Criando tests para as outras paginas

--- pypro/base/tests/test_about.py
+++ pypro/base/tests/test_about.py
@@ -2,5 +2,5 @@ from django.test import Client
 
 
 def test_status_code(client: Client):
-    resp = client.get('/rango/about')
-    assert resp.status_code == 301
+    resp = client.get('rango/about/')
+    assert resp.status_code == 200
