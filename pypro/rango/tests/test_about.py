a850797ff82c337e5d70918857c5dbe01f7572d2
andremenezees <andrr96@gmail.com>
2020-07-21 04:43:22 -0300

    Testes

--- pypro/rango/tests/test_about.py
+++ pypro/rango/tests/test_about.py
@@ -1,6 +1,19 @@
+import pytest
+from django.test import Client
 from django.urls import reverse
 
+from pypro.django_assertions import assert_contains
 
-def test_status_code(client, db):
+
+@pytest.fixture
+def resp(client, db):
     resp = client.get(reverse('rango:about'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Cat</title>')
--- pypro/rango/tests/test_index.py
+++ pypro/rango/tests/test_index.py
@@ -1,7 +1,24 @@
+import pytest
 from django.test import Client
 from django.urls import reverse
 
+from pypro.django_assertions import assert_contains
 
-def test_status_code(client: Client):
+
+@pytest.fixture
+def resp(client, db):
     resp = client.get(reverse('rango:index'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Rango</title>')
+
+
+def test_index_link(resp):
+    assert_contains(resp, f'href="{reverse("rango:index")}">Home</a>')
+
